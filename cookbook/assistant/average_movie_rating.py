4e632d35509fc6da9acab5dc80e0f08f7f85a312
balpreetcode <balpreet.code@gmail.com>
2024-03-08 18:10:04 +0000

    uyyh

--- cookbook/assistant/average_movie_rating.py
+++ cookbook/assistant/average_movie_rating.py
@@ -1,14 +1,14 @@
 import pandas as pd
 
-# Load the data
-url = 'https://phidata-public.s3.amazonaws.com/demo_data/IMDB-Movie-Data.csv'
-data = pd.read_csv(url)
+def calculate_average_rating(file_path):
+    # Load the dataset
+    df = pd.read_csv(file_path)
+    
+    # Calculate the average movie rating
+    average_rating = df['Rating'].mean()
+    return average_rating
 
-# Calculate the average rating
-average_rating = data['Rating'].mean()
-
-# Print the average rating
-print('The average rating of movies is:', average_rating)
-
-if __name__ == '__main__':
-    average_rating
+if __name__ == "__main__":
+    file_path = "./IMDB-Movie-Data.csv"
+    average = calculate_average_rating(file_path)
+    print(f"The average movie rating is: {average:.2f}")
--- cookbook/assistant/python_assistant.py
+++ cookbook/assistant/python_assistant.py
@@ -1,11 +1,11 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,Query
 import time
 from phi.assistant.python import PythonAssistant
 from phi.file.local.csv import CsvFile
 
 app = FastAPI()
 @app.get("/process-query/")
-async def process_query():
+async def process_query(question: str = Query(..., description="The question to ask")):
     try:
         # Hardcoded data
         start_time=time.time()
@@ -19,7 +19,7 @@ async def process_query():
             pip_install=True,
             show_tool_calls=True,
         )
-        response = python_assistant.run("What is the average rating of movies?",stream=False)
+        response = python_assistant.run(question,stream=False)
         return {"response": response,"time":time.time()-start_time}
     except Exception as e:
         print(f"An error occurred: {e}")
